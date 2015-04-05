00f4fc6f407af7dce69a1cd41ef24a136a83d3fc
Tristan Freeman <tristan.freeman@ymail.com>
2015-04-05 10:32:46 -0400

    fixed the issue with the add wine action bar icon displaying twice

--- WineHound/app/src/main/java/com/example/tristanfreeman/winehound/WineListActivity.java
+++ WineHound/app/src/main/java/com/example/tristanfreeman/winehound/WineListActivity.java
@@ -36,12 +36,12 @@ public class WineListActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wine_list, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_wine_list, menu);
+//        return true;
+//    }
 
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
